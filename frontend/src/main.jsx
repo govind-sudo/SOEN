9f964ea810172c2f8064ac4695546d3e3f622821
govind-sudo <govindbhadoriya668@gmail.com>
2026-08-18 01:25:54 +0530

    Fix frontend highlightjs assets

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'remixicon/fonts/remixicon.css'
 import App from './App.jsx'
+import 'highlight.js/styles/nord.css';
+import hljs from 'highlight.js';
 
 createRoot(document.getElementById('root')).render(
 
