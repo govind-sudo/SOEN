2fe828e3edcf97fd4ada734aa3ea65c35798070e
govind-sudo <govindbhadoriya668@gmail.com>
2026-08-18 01:44:59 +0530

    Fix frontend auth token handling

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -7,9 +7,14 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
 
+    console.log("TOKEN:", token);
+    console.log("REQUEST:", config.url);
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
+    console.log("AUTH HEADER:", config.headers.Authorization);
 
     return config;
 });
