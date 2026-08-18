791baf9a5da47e53423ac5b9cb9e29936f4b0efe
govind-sudo <govindbhadoriya668@gmail.com>
2026-08-18 17:51:46 +0530

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,7 +6,6 @@ import * as authMiddleware from '../middleware/auth.middleware.js';
 const router = Router();
 
 
-
 router.post('/register',
     body('email').isEmail().withMessage('Email must be a valid email address'),
     body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
